398ca220c510443ca11f2f6554ca3e5bfa38c85d
Ashik <Ashik@DESKTOP-H8OBQJK>
2023-05-02 12:22:21 +0530

    First Commit

---
+++ forLoop/src/forLoop/testLoops.java
@@ -0,0 +1,12 @@
+package forLoop;
+
+public class testLoops {
+  public static void main(String[] args) {
+	 for (int c1=0; c1<3; c1++ ) {
+		System.out.println("class NO :" +c1); 
+	 } for(int st=0; st<5; st++) {
+		 System.out.println("student NO :"+st);
+	 }
+	  
+  }
+}
